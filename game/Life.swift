6b77ede5aadb43ce17a0e66236180c40e33a2705
Glenn Wiley <glennwiley@gmail.com>
2016-07-17 21:49:10 -0500

    adding code to check neighbor cells

--- game/Life.swift
+++ game/Life.swift
@@ -14,10 +14,40 @@ class Life {
     
     init() {
         cells = [Cell]()
-        for xLoc in 0..<self.gridSize {
-            for yLoc in 0..<self.gridSize {
+        cells = assignCellsToGrid()
+    }
+    
+    lazy var assignCellsToGrid: () -> [Cell] = {
+        [weak self] in
+        var cells = [Cell]()
+        for xLoc in 0..<self!.gridSize {
+            for yLoc in 0..<self!.gridSize {
                 cells.append(Cell(x: xLoc, y: yLoc))
             }
         }
+        return cells
+    }
+    
+    func cellNeighbors(cell: Cell) -> [Cell] {
+        var neighbors : [Cell] = []
+        for cell2 in cells {
+            
+            let a = abs(cell.xCoord - cell2.xCoord)
+            let b = abs(cell.yCoord - cell2.yCoord)
+            
+            var check = false
+            switch (a, b) {
+            case (1,1), (1,0), (0,1):
+                check = true
+            default:
+                check = false
+            }
+            
+            if check == true {
+                neighbors.append(cell2)
+            }
+        }
+        
+        return neighbors
     }
 }
